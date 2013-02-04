bf75ab69b1d269c82c1d195b561f8daa33dbc862
Jim Lavin <jlavin@jimlavin.net>
2013-02-04 13:51:36 -0600

    Added Chapter4 Source Code

---
+++ Chapter04/app/home/home-controller-unit-tests.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jim.lavin
+ * Date: 12/20/12
+ * Time: 3:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ Chapter04/app/home/home-controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Application.Controllers.controller('home-controller', ['$scope', function($scope) {
+
+    $scope.isBrewerLoggedIn = function() {
+        return false;
+    };
+}]);
