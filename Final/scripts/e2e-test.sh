deb0af4f8d289955dbcc16e3c06d4d6c0616722c
lavinjj <jlavin@jimlavin.net>
2013-03-15 09:01:18 -0500

    changes to get test scripts to work under Linux

--- Final/scripts/e2e-test.sh
+++ Final/scripts/e2e-test.sh
@@ -5,5 +5,6 @@ BASE_DIR=`dirname $0`
 echo ""
 echo "Starting Testacular Unit Server (http://vojtajina.github.com/testacular)"
 echo "------------------------------------------------------------------------"
+echo $BASE_DIR
 
-testacular start $BASE_DIR/../config/testacular-unit.conf.js $*
+testacular start $BASE_DIR/../config/testacular-scenario.conf.js $*
